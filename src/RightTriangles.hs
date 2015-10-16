cca04e2896591210a030bed31017e157f76b46fb
SugimotoSohei <1130071@stu.hus.ac.jp>
2015-10-16 16:56:56 +0900

    Right修正

--- src/RightTriangles.hs
+++ src/RightTriangles.hs
@@ -1,5 +1,6 @@
 module Main where
 
+rightTriangle :: Int -> [(Int,Int,Int)]
 rightTriangle n = [(a,b,c) | c <- [1..n], a <- [1..c] , b <- [1..a], (a^2) + (b^2) == (c^2)]
 
 main = print $ rightTriangle 100
